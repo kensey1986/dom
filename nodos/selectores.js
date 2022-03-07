cb2071b747733d24dcd3f818a2c02ce8cb8a906b
Yivan2021 <manuel.rodriguez@softwareestrategico.com>
2022-03-07 11:50:49 -0500

    dom

---
+++ nodos/selectores.js
@@ -0,0 +1,29 @@
+//  const ids = document.getElementById('parrafo');
+//  console.log('==============document.getElementById();======================');
+//  console.log(ids.textContent);
+//  console.log('====================================');
+
+
+
+//   const etiquetas = document.getElementsByTagName('p')
+//   console.log('================document.getElementsByTagName()====================');
+//   console.log(etiquetas);
+//   console.log('====================================');
+
+//    const nombreClase = document.getElementsByClassName("prueba")
+//    console.log('================document.getElementsByClassName()====================');
+//    console.log(nombreClase);
+//    console.log('====================================');
+
+//    const etiquetaNombre = document.getElementsByName("miParrafo")
+//    console.log('================document.getElementsByName()====================');
+//    console.log(etiquetaNombre);
+//    console.log('====================================');
+
+   // const etiquetaNombre = document.querySelector('section');
+   const etiquetaNombre = document.querySelector('');
+   console.log('================document.querySelector()====================');
+   console.log(etiquetaNombre);
+   console.log('====================================');
+
+//  
